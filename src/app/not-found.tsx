42b1c2bc79279a41b624c3a09c34b2183696fa39
thanhtrung07 <40331826+thanhtrung07@users.noreply.github.com>
2025-05-06 21:07:01 +0700

    feat[OCP-1]: created base route

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div>
+            This is a 404 page. The page you are looking for does not exist.
+            <p className="text-lg">Please check the URL or go back to the homepage.</p>
+        </div>
+    )
+}

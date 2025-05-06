791270c2badd92a91375e4e245c2940a270720cd
thanhtrung07 <40331826+thanhtrung07@users.noreply.github.com>
2025-05-06 21:07:01 +0700

    [OCP-1] feat: created base route

---
+++ src/app/about-me/pages.tsx
@@ -0,0 +1,7 @@
+export default function AboutMe() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold underline">AboutMe</h1>
+      </div>
+    );
+  }
---
+++ src/app/skills/pages.tsx
@@ -0,0 +1,7 @@
+export default function Skills() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold underline">Skills</h1>
+      </div>
+    );
+  }

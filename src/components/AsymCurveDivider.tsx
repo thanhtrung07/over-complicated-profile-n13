b292a0cc2e66624ba196df5f8e5c4068490f2d37
thanhtrung07 <40331826+thanhtrung07@users.noreply.github.com>
2025-05-14 19:01:26 +0700

    feat[OCP-4]: Style and animated navigation bar

---
+++ src/components/AsymCurveDivider.tsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+interface IProps {
+  className?: string
+}
+
+export default function AsymCurveDivider({ className = '' }: IProps) {
+  return (
+    <div className={twMerge('hidden h-[16px] md:flex', '', className)}>
+      <div className="relative flex-1">
+        <div
+          className={clsx(
+            'absolute top-[7px] left-0 h-[9px] w-full rounded-tl-lg',
+            'border-border/40 dark:border-dk-border border-t-2'
+          )}
+        />
+      </div>
+      <div className="relative flex-1">
+        <div
+          className={clsx(
+            'absolute top-0 left-0 h-[9px] w-full rounded-br-lg',
+            'border-border/40 dark:border-dk-border border-b-2'
+          )}
+        ></div>
+      </div>
+    </div>
+  )
+}

4ee77ca619f4b9129f0161d3a44bbe8082cf68f1
thanhtrung07 <40331826+thanhtrung07@users.noreply.github.com>
2025-05-22 21:16:08 +0700

    feat[OCP-7]: Change layout on Board component and content position in viewport

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,20 +1,25 @@
-import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import RouteTitle from './RouteTitle'
 
 interface IProps {
-  children?: React.ReactNode
+  children: React.ReactNode
   className?: string
+  title: string
 }
 
-export default function Board({ children, className }: IProps) {
+export default function Board({ children, className, title }: IProps) {
   return (
     <div
-      className={clsx(
+      className={twMerge(
         'bg-foreground dark:bg-dark-foreground h-full w-full max-w-3xl rounded-xl px-4 py-5',
         'flex h-full w-full flex-col items-center gap-4',
         className
       )}
     >
-      {children ?? <></>}
+      <RouteTitle title={title} />
+      <div className="flex h-full min-h-[60vh] w-full flex-col justify-around gap-4 py-1">
+        {children}
+      </div>
     </div>
   )
 }

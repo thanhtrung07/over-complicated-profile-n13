95b3b9dea88d236d96d0f9e9430cc2eecf5e0fa3
thanhtrung07 <40331826+thanhtrung07@users.noreply.github.com>
2025-05-22 10:38:25 +0700

    feat[OCP-6]: Create Skills page

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,10 +1,19 @@
-export default function Board({
-  children,
-}: Readonly<{
+import clsx from 'clsx'
+
+interface IProps {
   children?: React.ReactNode
-}>) {
+  className?: string
+}
+
+export default function Board({ children, className }: IProps) {
   return (
-    <div className="bg-foreground dark:bg-dark-foreground h-full w-full rounded-xl p-4">
+    <div
+      className={clsx(
+        'bg-foreground dark:bg-dark-foreground h-full w-full max-w-3xl rounded-xl px-4 py-5',
+        'flex h-full w-full flex-col items-center gap-4',
+        className
+      )}
+    >
       {children ?? <></>}
     </div>
   )
